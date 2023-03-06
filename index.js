3596b8aef313c1a83f610ccb33382e13a75cbcf4
Diego <diego19martin@hotmail.com>
2023-03-06 16:11:59 -0300

    .

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ import extraccionesRoutes from "./routes/extracciones.routes.js"
 
 const app = express();
 
+app.use((req,res,next)=>{
+    res.setHeader('Access-Control-Allow-Origin','*');
+    res.setHeader('Access-Control-Allow-Methods','GET,POST,PUT,PATCH,DELETE');
+    res.setHeader('Access-Control-Allow-Methods','Content-Type','Authorization');
+    next(); 
+})
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 
-app.use(cors());
+
 app.use(express.json());
 app.use(extraccionesRoutes);
