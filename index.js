bfa9c53caebe72bf84f1dfa2276b3681a54f463c
Diego <diego19martin@hotmail.com>
2023-03-30 15:41:48 -0300

    .

--- index.js
+++ index.js
@@ -4,16 +4,7 @@ import extraccionesRoutes from "./routes/extracciones.routes.js"
 
 const app = express();
 
-const corsOption = {
-    credentials: true,
-    origin: ['https://extracciones-client-conversion.vercel.app']
-}
-
-res.setHeader("Access-Control-Allow-Origin", 'https://extracciones-client-conversion.vercel.app');
-res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS,PUT,DELETE');
-res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Accept');
-
-app.use(cors(corsOption));
+app.use(cors());
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
--- routes/extracciones.routes.js
+++ routes/extracciones.routes.js
@@ -1,13 +1,21 @@
 import { Router } from "express";
 import { getInfo, getResumen, postConfig, postList, postSelect } from "../controllers/extracciones.controller.js";
+import cors from "cors";
 
 const router = Router();
+
+
+const corsOption = {
+    credentials: true,
+    origin: ['https://extracciones-client-conversion.vercel.app']
+}
+
 
 router.post('/postmaquinas', postList);
 router.post('/postconfig/:limite', postConfig);
 router.post('/postSelect', postSelect);
-router.get('/getResumen', getResumen);
-router.get('/getInfo/:maquina', getInfo);
+router.get('/getResumen', getResumen, cors(corsOption));
+router.get('/getInfo/:maquina', getInfo, cors(corsOption));
 
 
 
