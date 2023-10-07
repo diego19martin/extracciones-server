cf0e5b2b264d0db18284e4c8cb07002d82addacf
Diego Argonz <72052494+diego19martin@users.noreply.github.com>
2023-10-07 13:19:10 +0000

    .

--- index.js
+++ index.js
@@ -4,17 +4,13 @@ import extraccionesRoutes from "./routes/extracciones.routes.js"
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 
-// Aumentar el límite de tamaño de carga a 10 MB
-app.use(bodyParser.json({ limit: '10mb' }));
-
+app.use(express.json({ limit: '10mb' }));
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
 });
 
-
-app.use(express.json());
 app.use(extraccionesRoutes);
