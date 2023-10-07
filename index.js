a46a0f3c3e1eeca9e0a63048bbdbb72ff5916a5b
Diego Argonz <72052494+diego19martin@users.noreply.github.com>
2023-10-07 13:10:45 +0000

    .

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const app = express();
 
 app.use(cors());
 
+// Aumentar el límite de tamaño de carga a 10 MB
+app.use(bodyParser.json({ limit: '10mb' }));
+
+
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Our app is running on port ${ PORT }`);
