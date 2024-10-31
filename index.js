fdbee300bfeea0751a6303d8419ab4e235ebeb36
Diego Argonz <diego19martin@hotmail.com>
2024-10-31 14:13:56 -0300

    .

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ cron.schedule('0 14 * * *', () => {
 
 //pruebas
 
-cron.schedule('11 17 * * *', () => {
-  console.log('Generando y enviando reporte diario a las 10 AM Buenos Aires (13:00 UTC)');
-  generarReporteResumen();
-});
+// cron.schedule('11 17 * * *', () => {
+//   console.log('Generando y enviando reporte diario a las 10 AM Buenos Aires (13:00 UTC)');
+//   generarReporteResumen();
+// });
 
-cron.schedule('11 17 * * *', () => {
-  console.log('Generando y enviando reporte técnico a las 11 AM Buenos Aires (14:00 UTC)');
-  generarYEnviarReporte('tecnica');
-});
+// cron.schedule('11 17 * * *', () => {
+//   console.log('Generando y enviando reporte técnico a las 11 AM Buenos Aires (14:00 UTC)');
+//   generarYEnviarReporte('tecnica');
+// });
